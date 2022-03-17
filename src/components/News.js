24ea8fbdb8e81df05519e78feed4bb3a60566051
ProMinato500 <nandip500@gmail.com>
2022-03-18 00:23:22 +0530

    added news and newsitems

--- src/components/News.js
+++ src/components/News.js
@@ -4,14 +4,19 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-          News
-          <NewsItem/>
-          <NewsItem/>
-          <NewsItem/>
-          <NewsItem/>
-          <NewsItem/>
-          <NewsItem/>
+      <div className='container my-3'>
+          <h2>NewsMonkey - Top Headlines</h2>
+          <div className="row">
+              <div className="col-md-3">
+                <NewsItem title="MyTitle" description="DESC"/>
+              </div>
+              <div className="col-md-3">
+                <NewsItem title="MyTitle" description="DESC"/>
+              </div>
+              <div className="col-md-3">
+                <NewsItem title="MyTitle" description="DESC"/>
+              </div>
+          </div>
       </div>
     )
   }
